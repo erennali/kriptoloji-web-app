cc624631273522a6434a2cd59c1b3ad3aff95942
Eren Ali Koca <eren_ali_koca@hotmail.com>
2025-05-10 23:18:27 +0300

    chore: proje klasörleri oluşturuldu

---
+++ KriptolojiWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KriptolojiWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
